111af7ba6191005677e1e427def7e7b19a88d078
shmzzzz <slt0623@gmail.com>
2024-04-30 16:01:02 +0900

    feat: MainActivityで呼び出す

--- app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.triviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.triviaapp.screen.QuestionsViewModel
 import com.example.triviaapp.ui.theme.TriviaAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,11 +21,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TriviaAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TriviaHome()
                 }
             }
         }
@@ -33,17 +32,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TriviaHome(
+    viewModel: QuestionsViewModel = hiltViewModel()
+) {
+    Questions(viewModel = viewModel)
+}
+
+@Composable
+fun Questions(
+    viewModel: QuestionsViewModel
+) {
+    val questions = viewModel.data.value.data?.toMutableList()
+
+    Log.d("SIZE", "Questions: ${questions?.size}")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     TriviaAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/triviaapp/screen/QuestionsViewModel.kt
+++ app/src/main/java/com/example/triviaapp/screen/QuestionsViewModel.kt
@@ -14,11 +14,11 @@ import javax.inject.Inject
 @HiltViewModel
 class QuestionsViewModel @Inject constructor(private val repository: QuestionRepository) :
     ViewModel() {
-    private val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
+    val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
         mutableStateOf(
             DataOrException(null, true, Exception(""))
         )
-    
+
     init {
         getAllQuestions()
     }
