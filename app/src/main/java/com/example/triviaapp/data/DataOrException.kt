bc36d6a8cf7d0290f29d9aafec087cec18b1956f
shmzzzz <slt0623@gmail.com>
2024-04-30 15:23:36 +0900

    feat: Repositoryのセットアップ

--- app/src/main/java/com/example/triviaapp/data/DataOrException.kt
+++ app/src/main/java/com/example/triviaapp/data/DataOrException.kt
@@ -3,5 +3,5 @@ package com.example.triviaapp.data
 data class DataOrException<T, Boolean, E : Exception>(
     var data: T? = null,
     var loading: Boolean? = null,
-    var exception: E? = null,
+    var e: E? = null,
 )
