dd12cfe3f10f70797f8b63f95882a6ac4546b76d
shmzzzz <slt0623@gmail.com>
2024-04-30 16:37:21 +0900

    feat: 破線のUIを作成する

--- app/src/main/java/com/example/triviaapp/screen/Questions.kt
+++ app/src/main/java/com/example/triviaapp/screen/Questions.kt
@@ -1,10 +1,12 @@
 package com.example.triviaapp.screen
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
@@ -12,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -43,6 +47,8 @@ fun Questions(
 @Preview
 @Composable
 fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -56,6 +62,7 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start,
         ) {
             QuestionTracker()
+            DrawDottedLine(pathEffect = pathEffect)
         }
 
     }
@@ -93,3 +100,20 @@ fun QuestionTracker(
         modifier = Modifier.padding(20.dp),
     )
 }
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp),
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect,
+        )
+    }
+}
+
