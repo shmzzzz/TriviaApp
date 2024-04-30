b580181ce5210f1c80230cc407b68e9ea5cecd8c
shmzzzz <slt0623@gmail.com>
2024-04-30 16:26:40 +0900

    feat: TrackerのUI作成

--- app/src/main/java/com/example/triviaapp/screen/Questions.kt
+++ app/src/main/java/com/example/triviaapp/screen/Questions.kt
@@ -1,7 +1,27 @@
 package com.example.triviaapp.screen
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.triviaapp.util.AppColors
 
 
 @Composable
@@ -10,6 +30,7 @@ fun Questions(
 ) {
     val questions = viewModel.data.value.data?.toMutableList()
     if (viewModel.data.value.loading == true) {
+        CircularProgressIndicator()
         Log.d("Loading", "Questions: Loading...")
     } else {
         Log.d("Loading", "Questions: Loading Stopped...")
@@ -17,4 +38,58 @@ fun Questions(
             Log.d("Result", "Questions: ${questionItem.question}")
         }
     }
+}
+
+@Preview
+@Composable
+fun QuestionDisplay() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(4.dp),
+        color = AppColors.mDarkPurple,
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+        ) {
+            QuestionTracker()
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun QuestionTracker(
+    counter: Int = 10,
+    outOf: Int = 100
+) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+                ) {
+                    append("Question $counter/")
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppColors.mLightGray,
+                            fontWeight = FontWeight.Light,
+                            fontSize = 14.sp
+                        )
+                    ) {
+                        append("$outOf")
+                    }
+                }
+            }
+        },
+        modifier = Modifier.padding(20.dp),
+    )
 }
--- app/src/main/java/com/example/triviaapp/screen/TriviaHome.kt
+++ app/src/main/java/com/example/triviaapp/screen/TriviaHome.kt
@@ -2,7 +2,6 @@ package com.example.triviaapp.screen
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.triviaapp.Questions
 
 @Composable
 fun TriviaHome(
